99e1648aa6b873505a15ed06c26038fe5ad3e3ec
pancholiraj27 <75990574+pancholiraj27@users.noreply.github.com>
2021-06-29 22:49:27 +0530

    initial commit

---
+++ windows 11 dummy/script.js
@@ -0,0 +1,12 @@
+let taskbar = document.getElementsByClassName("taskbar")[0]
+let startMenu = document.getElementsByClassName("startMenu")[0]
+
+taskbar.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(startMenu.style.bottom == "50px"){
+        startMenu.style.bottom = "-655px"
+    }
+    else{
+        startMenu.style.bottom = "50px"
+    }
+})
